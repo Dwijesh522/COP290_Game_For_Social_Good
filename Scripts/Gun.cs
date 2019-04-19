d0cdf7dc4e7d7b2eb55d0c66381920de23db9f95
dwijesh522 <iddwijesh@gmail.com>
2019-04-20 00:08:23 +0530

    Learnt how to use scripts in unity

---
+++ Scripts/Gun.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public Transform launchPosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Fire1") == 1.0f)
+        {
+            if(!IsInvoking("FireBullet"))
+            {
+                InvokeRepeating("FireBullet", 0.0f, 0.1f);
+            }
+        }
+
+        if(Input.GetAxis("Fire1") != (1.0f))
+        {
+            CancelInvoke("FireBullet");
+        }
+	}
+
+    void FireBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = launchPosition.position;
+        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+    }
+}
---
+++ Scripts/PlayerControler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float moveSpeed = 50.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position = pos;
+    }
+}
