1c4df6adf2b6a1902983c96cd4dc320255b42c7e
dwijesh522 <iddwijesh@gmail.com>
2019-04-21 19:19:41 +0530

    Learned creating animations, transition_states, connecting scripts to animation

---
+++ practice codes/animation_code_scripts/CollisionBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBehavior : MonoBehaviour {
+    public bool collided;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(!collided)
+        {
+            collided = true;
+            GameObject clown = gameObject.transform.parent.gameObject;
+            Animator clownAnimator = clown.GetComponent<Animator>();
+            clownAnimator.SetTrigger("Hit");
+        }
+    }
+}
